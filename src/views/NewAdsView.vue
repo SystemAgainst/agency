1776137db0214029d82fb26a1676696b72a0ab4c
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2023-11-28 15:53:44 +0300

    feature: add saving data from the form to localeStorage

--- src/views/NewAdsView.vue
+++ src/views/NewAdsView.vue
@@ -1,73 +1,72 @@
 <script setup>
-import { store } from "@/store/index.js";
+import { useAdsStore } from "@/store/index.js";
 import {ref} from "vue";
+import {router} from "@/router/index.js";
 
-const useAdsStore = store();
+const adsStore = useAdsStore();
 
-const formData = ref({
-  name: "",
-  inn: "",
-  address: "",
-  bik: null,
-  currentAccount: null,
-  correspondentAccount: null,
-});
+const name = ref("");
+const inn = ref("");
+const address = ref("");
+const bik = ref(null);
+const currentAccount = ref(null);
+const correspondentAccount = ref(null);
 
-const addAds = () => {
-  useAdsStore
+const resetForm = () => {
+  name.value = '';
+  inn.value = '';
+  address.value = '';
+  bik.value = null;
+  currentAccount.value = null;
+  correspondentAccount.value = null;
+}
+
+const handleSubmit = () => {
+  adsStore.addAds({
+    name: name,
+    inn: inn,
+    address: address,
+    bik: bik,
+    currentAccount: currentAccount,
+    correspondentAccount: correspondentAccount,
+  });
+
+  resetForm();
+
+  router.push('/');
 };
 </script>
 
 <template>
-  <!--
-  <div class="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-
-    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-      Read more
-      <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-      </svg>
-    </a>
-  </div>
-  -->
   <h2 class="text-2xl font-bold">Создание рекламной компании (РК)</h2>
 
-  <form class="mt-16" @submit.prevent="addAds()">
+  <form class="mt-16" @submit.prevent="handleSubmit()" @keydown.enter="handleSubmit()">
     <div class="grid gap-6 mb-6 md:grid-cols-2">
       <div>
         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Наименование</label>
-        <input v-model="formData.name" type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ООО Таргет" required>
+        <input v-model="name" type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ООО Таргет" required>
       </div>
       <div>
         <label for="inn" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ИНН</label>
-        <input v-model="formData.inn" type="text" id="inn" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required>
+        <input v-model="inn" type="text" id="inn" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
       </div>
       <div>
         <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Адрес</label>
-        <input v-model="formData.address" type="text" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        <input v-model="address" type="text" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
       </div>
       <div>
         <label for="bik" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">БИК</label>
-<!--        <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required>-->
-        <input v-model="formData.bik" type="number" id="bik" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+        <input v-model="bik" type="number" id="bik" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
       </div>
       <div>
         <label for="currentAccount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Расчетный счет</label>
-        <input v-model="formData.currentAccount" type="number" id="currentAccount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+        <input v-model="currentAccount" type="number" id="currentAccount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
       </div>
       <div>
         <label for="correspondentAccount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Корреспондентский счет</label>
-        <input v-model="formData.correspondentAccount" type="number" id="correspondentAccount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+        <input v-model="correspondentAccount" type="number" id="correspondentAccount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
       </div>
     </div>
-<!--    <div class="flex items-start mb-6">-->
-<!--      <div class="flex items-center h-5">-->
-<!--        <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>-->
-<!--      </div>-->
-<!--      <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>-->
-<!--    </div>-->
     <button
       type="submit"
       class="justify-items-end text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
